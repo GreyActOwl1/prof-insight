31d6dd15e4140f21f007afa2c55e58b8d1fa98a1
aryankumar2828 <aryan.nair.personal@gmail.com>
2024-08-29 12:44:19 -0600

    Made Changes to Template
    Removed:
    1) Info button and Modal
    2) Side Panel with info
    
    Modified:
    1) Chat Window - Made it bigger and centred
    2) Title - Changed to "Professor Insights"

--- src/app/components/Chat/index.tsx
+++ src/app/components/Chat/index.tsx
@@ -18,26 +18,30 @@ const Chat: React.FC<Chat> = ({
   messages,
 }) => {
   return (
-    <div id="chat" className="flex flex-col w-full lg:w-3/5 mr-4 mx-5 lg:mx-0">
-      <Messages messages={messages} />
-      <>
-        <form
-          onSubmit={handleMessageSubmit}
-          className="mt-5 mb-5 relative bg-gray-700 rounded-lg"
-        >
-          <input
-            type="text"
-            className="input-glow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline pl-3 pr-10 bg-gray-600 border-gray-600 transition-shadow duration-200"
-            value={input}
-            onChange={handleInputChange}
-          />
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full rounded-lg shadow-lg overflow-hidden">
+        <div className="flex flex-col h-[600px]">
+          <Messages messages={messages} />
+          <>
+            <form
+              onSubmit={handleMessageSubmit}
+              className="mt-5 mb-5 relative bg-gray-700 rounded-lg"
+            >
+              <input
+                type="text"
+                className="input-glow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline pl-3 pr-10 bg-gray-600 border-gray-600 transition-shadow duration-200"
+                value={input}
+                onChange={handleInputChange}
+              />
 
-          <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none text-gray-400">
-            Press ⮐ to send
-          </span>
-        </form>
-      </>
-    </div>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none text-gray-400">
+                Press ⮐ to send
+              </span>
+            </form>
+          </>
+          </div>
+        </div>
+      </div>
   );
 };
 
